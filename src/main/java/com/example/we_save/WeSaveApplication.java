662e31ccdbf801dc4d714c2f5812b15065859095
aerim-choi <80438964+aerim-choi@users.noreply.github.com>
2024-07-31 23:13:33 +0900

    init. 프로젝트 초기셋팅

---
+++ src/main/java/com/example/we_save/WeSaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.we_save;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeSaveApplication.class, args);
+	}
+
+}
