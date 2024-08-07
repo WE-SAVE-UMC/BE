1fce1f910f85a3ec73ba345fd9ab2711127e811b
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-08-07 19:55:16 +0900

    feature: 마이페이지 프로필 조회 API 구현 완료
    - 로그인 된 회원만 정보를 볼 수 있도록 토큰을 이용하여 프로필 조회하도록 함
    - 로그인 로직 리팩토링(번호 중복 확인 로직 추가)

--- src/main/java/com/example/we_save/domain/user/controller/UserAuthController.java
+++ src/main/java/com/example/we_save/domain/user/controller/UserAuthController.java
@@ -11,12 +11,17 @@ import com.example.we_save.domain.user.service.NotificationSettingCommandService
 import com.example.we_save.domain.user.service.UserAuthCommandService;
 import com.example.we_save.jwt.JWTUtil;
 import com.example.we_save.jwt.service.CustomUserDetailsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Security;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,14 +31,23 @@ public class UserAuthController {
     private final NotificationSettingCommandService notificationSettingCommandService;
     private final JWTUtil jwtUtil;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/api/auth/users")
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse<UserAuthResponseDto.JoinResultDto> join(@RequestBody @Valid UserAuthRequestDto.JoinDto request){
+    public ResponseEntity<ApiResponse<UserAuthResponseDto.JoinResultDto>> join(@RequestBody @Valid UserAuthRequestDto.JoinDto request){
         NotificationSetting notificationSetting = notificationSettingCommandService.createNotificationSetting();
         User user= userAuthCommandService.joinUser(request,notificationSetting);
-        return ApiResponse.onPostSuccess(UserConverter.toJoinResultDto(user), SuccessStatus._POST_OK);
+
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(ApiResponse.onFailure("COMMON409","409 Confilt, 이미 회원가입이 된 회원입니다.",null));
+        }
+        else{
+            ApiResponse<UserAuthResponseDto.JoinResultDto> response = ApiResponse.onPostSuccess(UserConverter.toJoinResultDto(user), SuccessStatus._POST_OK);
+            return ResponseEntity.ok(response);
+        }
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/api/auth/login")
     public ResponseEntity<ApiResponse> login(@RequestBody @Valid UserAuthRequestDto.loginDto request){
         User user = userAuthCommandService.loginUser(request);
@@ -42,12 +56,30 @@ public class UserAuthController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.onFailure("COMMON401","401 Unauthorized, 인증 정보가 잘못되었습니다.",user));
         }
         else{
-            String token = jwtUtil.createJwt(request.getPhoneNum(),"USER",24 * 60 * 60 * 1000L);
+            String token = jwtUtil.createJwt(user.getId().toString(),"USER",24 * 60 * 60 * 1000L);
             ApiResponse<UserAuthResponseDto.loginResultDto> response = ApiResponse.onPostSuccess(UserConverter.toLoginResultDto(user,token),SuccessStatus._POST_OK);
             return ResponseEntity.ok(response);
         }
     }
 
+
+    @Operation(summary = "로그인 된 회원 정보(마이페이지-프로필 조회)", security = @SecurityRequirement(name="Authorization"))
+    @GetMapping("/api/auth/users")
+    public ResponseEntity<ApiResponse> findUser(){
+        long userId = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getName());
+
+        User user = userAuthCommandService.findByUserId(userId);
+
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.onFailure("COMMON401","401 Unauthorized, 인증 정보가 잘못되었습니다.",user));
+        }
+        else{
+            ApiResponse<UserAuthResponseDto.findUserResultDto> response = ApiResponse.onGetSuccess(UserConverter.toUserResultDto(user));
+            return ResponseEntity.ok(response);
+        }
+    }
+
+    @Operation(summary = "유효한 전화번호 확인")
     @GetMapping("/api/auth/check-phone/{number}")
     public ResponseEntity<ApiResponse<UserAuthResponseDto.ValidResultDto>>isValidPhoneNum(@PathVariable String number){
         Boolean isValid = userAuthCommandService.isValidPhoneNumber(number);
@@ -63,6 +95,8 @@ public class UserAuthController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
         }
     }
+
+    @Operation(summary = "유효한 닉네임 확인")
     @GetMapping("/api/auth/check-nickname/{nickname}")
     public ResponseEntity<ApiResponse<UserAuthResponseDto.ValidResultDto>>isValidNickame(@PathVariable String nickname){
         Boolean isValid = userAuthCommandService.isValidNickname(nickname);
