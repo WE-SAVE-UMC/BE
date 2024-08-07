09f009193e955f29865b898d708ae210034ab58d
nicehcy <nicehcy2@naver.com>
2024-08-07 22:37:19 +0900

    feature: 관심지역 Entity 추가

---
+++ src/main/java/com/example/we_save/domain/region/entity/HeartRegion.java
@@ -0,0 +1,26 @@
+package com.example.we_save.domain.region.entity;
+
+import com.example.we_save.apiPayload.code.BaseEntity;
+import com.example.we_save.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class HeartRegion extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private EupmyeondongRegion region;
+}
