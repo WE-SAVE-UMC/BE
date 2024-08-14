cdc9e69d797a1b9a5bf88f3589d988111393f2f3
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-08-14 14:43:04 +0900

    docs: post이미지 등록 삭제 api 예시 코드

--- src/main/java/com/example/we_save/image/service/ImageService.java
+++ src/main/java/com/example/we_save/image/service/ImageService.java
@@ -1,12 +1,17 @@
 package com.example.we_save.image.service;
 
+import com.example.we_save.domain.post.entity.Post;
 import com.example.we_save.domain.user.entity.User;
 import com.example.we_save.image.entity.Image;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface ImageService {
     public Image saveProfileImage(MultipartFile file, User user) throws IOException;
     public void deleteProfileImage(long imageId,User user) throws IOException ;
+    public List<Image> savePostImages(List<MultipartFile> files, Post post) throws IOException;
+    public void deletePostImage(long imageId,Post post) throws IOException;
+    public void deletePostAllImage(Post post) throws IOException;
 }
--- src/main/java/com/example/we_save/image/service/ImageServiceImpl.java
+++ src/main/java/com/example/we_save/image/service/ImageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.we_save.image.service;
 
+import com.example.we_save.domain.post.entity.Post;
 import com.example.we_save.domain.user.entity.User;
 import com.example.we_save.domain.user.repository.UserRepository;
 import com.example.we_save.image.entity.Image;
@@ -13,6 +14,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -70,5 +73,42 @@ public class ImageServiceImpl implements ImageService {
         if (image != null) {
             imageRepository.delete(image);
         }
+    }
+
+    @Override
+    public List<Image> savePostImages(List<MultipartFile> files, Post post) throws IOException {
+        String projectPath = "/home/upload/post/"+post.getId();
+        Path directoryPath = Paths.get(projectPath);
+        if (Files.notExists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+        }
+        List<Image> savedImages = new ArrayList<>();
+
+        // Save each file
+        for (MultipartFile file : files) {
+            UUID uuid = UUID.randomUUID();
+            String fileName = uuid + "_" + file.getOriginalFilename();
+            File saveFile = new File(projectPath, fileName);
+            file.transferTo(saveFile);
+
+            Image image = new Image();
+            image.setName(fileName);
+            image.setFilePath("/files/post/" + post.getId() + fileName);
+            image = imageRepository.save(image);
+
+            savedImages.add(image);
+        }
+
+        return savedImages;
+    }
+
+    @Override
+    public void deletePostImage(long imageId, Post post) throws IOException {
+
+    }
+
+    @Override
+    public void deletePostAllImage(Post post) throws IOException {
+
     }
 }
