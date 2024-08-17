01f2b8e7c69095b7494be42292cb5747fa014674
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-08-17 17:20:55 +0900

    fix: 이미지 등록 안했을 시 오류 해결
    - 이미지 등록 안했을 경우 디폴트 이미지로 변경되게 오류 해결

--- src/main/java/com/example/we_save/domain/user/controller/UserAuthController.java
+++ src/main/java/com/example/we_save/domain/user/controller/UserAuthController.java
@@ -83,13 +83,28 @@ public class UserAuthController {
                                                       @RequestPart(value = "profileImage",required = false) MultipartFile profileImage ){
         User user = userAuthCommandService.getAuthenticatedUserInfo();
         try {
-            imageService.deleteProfileImage(user.getProfileImage().getId(),user); //파일서버에서 기존 프로필 이미지 삭제
-            Image newProfileImage = imageService.saveProfileImage(profileImage,user); //파일서버에 프로필 이미지 등록
+
+            imageService.deleteProfileImage(user.getProfileImage().getId(), user); //파일서버에서 기존 프로필 이미지 삭제
+            Image newProfileImage;
+
+            if (profileImage == null || profileImage.isEmpty()) {
+                newProfileImage = imageService.saveDefaultProfileImage(); //이미지 등록 안할시 디폴트 이미지 등록
+            } else {
+                newProfileImage = imageService.saveProfileImage(profileImage, user); //파일서버에 프로필 이미지 등록
+            }
             User updateUser = userAuthCommandService.updateUser(user, nickname, newProfileImage); //유저 정보 업데이트
             ApiResponse<UserAuthResponseDto.findUserResultDto> response = ApiResponse.onGetSuccess(UserConverter.toUserResultDto(updateUser));
             return ResponseEntity.ok(response);
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.onFailure("COMMON400","파일 업로드 오류",null));
+        }
+//        }catch (IllegalArgumentException e){
+//            Image newProfileImage = imageService.saveDefaultProfileImage();
+//            User updateUser = userAuthCommandService.updateUser(user, nickname, newProfileImage); //유저 정보 업데이트
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.onFailure("COMMON400","profileImage값을 요청받지 못한 오류",UserConverter.toUserResultDto(updateUser)));
+//        }
+        catch (Exception e){
+            Image newProfileImage = imageService.saveDefaultProfileImage();
+            User updateUser = userAuthCommandService.updateUser(user, nickname, newProfileImage); //유저 정보 업데이트
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.onFailure("COMMON400","파일 업로드 오류",UserConverter.toUserResultDto(updateUser)));
         }
     }
 
