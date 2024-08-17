0346ffbeec3660eefb3f7260958cf9c9456e7dda
nicehcy <nicehcy2@naver.com>
2024-08-17 21:33:35 +0900

    Feat: 해시 태그 기반 검색 구현

--- src/main/java/com/example/we_save/domain/countermeasure/entity/CountermeasureHashTag.java
+++ src/main/java/com/example/we_save/domain/countermeasure/entity/CountermeasureHashTag.java
@@ -1,14 +1,21 @@
 package com.example.we_save.domain.countermeasure.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class CountermeasureHashTag {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private long id = 0L;
 
-    @Column(nullable = false, length = 10)
-    private String tagName;
+    @ManyToOne
+    @JoinColumn(name = "countermeasure_id", nullable = false)
+    private Countermeasure countermeasure;
+
+    @ManyToOne
+    @JoinColumn(name = "hashtag_id", nullable = false)
+    private HashTag hashTag;
 }
--- src/main/java/com/example/we_save/domain/countermeasure/repository/CountermeasureHashTagRepository.java
+++ src/main/java/com/example/we_save/domain/countermeasure/repository/CountermeasureHashTagRepository.java
@@ -3,5 +3,9 @@ package com.example.we_save.domain.countermeasure.repository;
 import com.example.we_save.domain.countermeasure.entity.CountermeasureHashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CountermeasureHashTagRepository extends JpaRepository<CountermeasureHashTag, Long> {
+
+    List<CountermeasureHashTag> findAllByHashTag_TagName(String hashTag);
 }
