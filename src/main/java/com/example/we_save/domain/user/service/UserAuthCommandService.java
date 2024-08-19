f11b889ad6b9fb0d2619da37c7afd1b9630bc370
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-08-19 18:31:57 +0900

    feature&refactor: 로그인 Controller 수정 및 회원탈퇴 API 구현

--- src/main/java/com/example/we_save/domain/user/service/UserAuthCommandService.java
+++ src/main/java/com/example/we_save/domain/user/service/UserAuthCommandService.java
@@ -5,16 +5,27 @@ import com.example.we_save.domain.user.entity.NotificationSetting;
 import com.example.we_save.domain.user.entity.User;
 import com.example.we_save.image.entity.Image;
 
-import java.util.Optional;
 
 public interface UserAuthCommandService {
-    public User joinUser(UserAuthRequestDto.JoinDto request, NotificationSetting notificationSetting,Image image);
+    public User joinUser(UserAuthRequestDto.JoinDto request, NotificationSetting notificationSetting, Image image);
+
     public User loginUser(UserAuthRequestDto.loginDto request);
+
     public Boolean isValidPhoneNumber(String phoneNumber);
+
     public Boolean isValidNickname(String nickname);
+
     public User findByUserId(long userId);
+
     public User updateUser(User user, String newNickname, Image newProfileImage);
+
+    public void inactiveUser(User user);
+
+    public void activateUser(User user);
+
     public User getAuthenticatedUserInfo();
+
     public User findByUserPhoneNumber(String phoneNumber);
+
     public User updateUserPassword(User user, String newPassword);
 }
